9a2a798771499beb5044d02667f5f2ac0ea23694
Camberden <42500247+Camberden@users.noreply.github.com>
2025-12-19 07:22:45 -0500

    broken site; configured ssh for file transfer though

--- preprocessor.js
+++
@@ -1,8 +0,0 @@
-function redirectPHP(){
-	// const phpAccess = document.getElementById("php-access");
-	window.onload = function(e){
-		e.preventDefault();
-		document.location = "/index.php";
-	}
-}
-// redirectPHP();
